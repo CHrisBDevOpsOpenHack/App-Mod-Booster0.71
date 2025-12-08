cfd43a52f29979bb1a747ddb83d45d1821fbb53e
Chris Bennett <christopher.bennett@microsoft.com>
2025-12-08 17:06:53 +0000

    Revert "Merge pull request #3 from CHrisBDevOpsOpenHack/copilot/modernise-my-app-again"
    This reverts commit 5d896bf8d375439a68a00a24e0f60063f818ed6d, reversing
    changes made to 28b8d0ce94f03c4816125aca051b9be97b559101.

--- src/ExpenseManagement/Models/ErrorInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ExpenseManagement.Models;
-
-public class ErrorInfo
-{
-    public string Message { get; set; } = "";
-    public string? Details { get; set; }
-    public string? Location { get; set; }
-    public string? Guidance { get; set; }
-}
